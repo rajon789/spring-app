9828e05945ca951ed993deea2513958ed9f90ebd
Rajon <rajon@example.com>
2021-06-19 04:07:43 +0600

    no change commit

--- src/main/java/com/t24/middleware/t24middleware/student/StudentController.java
+++ src/main/java/com/t24/middleware/t24middleware/student/StudentController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "api/v1")
+@RequestMapping(path = "api/v1/student")
 public class StudentController {
 
     private final studentService studentService;
