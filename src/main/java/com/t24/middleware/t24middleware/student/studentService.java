993f14cb0fcea9bd6c183b2c8dcf0dd2cdb5d8af
Rajon <rajon@example.com>
2021-06-21 22:19:59 +0600

    delete and put

--- src/main/java/com/t24/middleware/t24middleware/student/studentService.java
+++ src/main/java/com/t24/middleware/t24middleware/student/studentService.java
@@ -3,10 +3,12 @@ package com.t24.middleware.t24middleware.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import javax.xml.bind.SchemaOutputResolver;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -33,5 +35,30 @@ public class studentService {
             throw new IllegalStateException("Email Taken");
         }
         studentRepository.save(student);
+    }
+
+    public void deleteStudent(Long studentId) {
+        boolean exists = studentRepository.existsById(studentId);
+        if (!exists) {
+            throw new IllegalStateException("Student with this id" + studentId + "does not exists");
+        }
+        studentRepository.deleteById(studentId);
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+        Student student = studentRepository.findById(studentId).orElseThrow(() -> new IllegalStateException("Student with id " + studentId + " does not exists"));
+
+        Optional<Student> studentOptional = studentRepository.findByEmail(email);
+        if(studentOptional.isPresent()) {
+            throw  new IllegalStateException("Email Taken");
+        }
+        if (name != null && name.length() > 0 && !Objects.equals(student.getName(), name)) {
+            student.setName(name);
+        }
+
+        if(email != null && email.length() > 0 && ! Objects.equals(student.getEmail(),email)) {
+            student.setEmail(email);
+        }
     }
 }
